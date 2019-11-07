f291babb15039c364171546ec7615436122645e4
AussieGrid <38469955+AussieGrid@users.noreply.github.com>
2019-11-07 23:51:56 +0700

    Update genetic_method.py

--- genetic_method.py
+++ genetic_method.py
@@ -12,7 +12,7 @@ class Bot:
 
     success = False
 
-    def __init__(self, genes = []):
+    def __init__(self, genes):
         # Bot constructor - creates 'genes sequence' of a particular Bot
         # конструктор Bot - создает "последовательность генов" для отдельного бота
         for i in range(len(genes)):
@@ -21,29 +21,29 @@ class Bot:
     def __init__(self, number_of_genes, max_value, min_value, target, eps):
         # Bot constructor overload - creates 'genes' for a bot in random manner
         # перегрузка конструктора Bot - создает "гены" для ботов рандомно
-        self.target = target
-        self.eps = eps
+        self._target = target
+        self._eps = eps
 
         for i in range(number_of_genes):
-            _x_value = random.random()
-            self._genes = max_value * _x_value + (1 - _x_value) * min_value
+            __x_value = random.random()
+            self._genes = max_value * __x_value + (1 - __x_value) * min_value
 
     def get_fitness(self):
         # Check if bot`s genes meet conditions of precision (eps)
         # Проверка, подходит ли набор генов бота для нужной точности
-        fitness = abs(self._genes[0] + 2 * self._genes[1] + 3 * self._genes[3] + 4 * self._genes[4] - self.target)
-        if fitness <= self.eps:
+        fitness = abs(self._genes[0] + 2 * self._genes[1] + 3 * self._genes[3] + 4 * self._genes[4] - self._target)
+        if fitness <= self._eps:
             self.success = True
         return fitness
 
     def mutation(self):
         # mutate bot - change genes of one a little
         # мутация бота - немножко меняются значения его генов
-        _r_value = random(10)
-        _p_value = random(100)
+        __r_value = random.random(10)
+        __p_value = random.random(100)
 
         for i in range(self.number_of_genes):
-            self._genes[i] = self._genes[i] * (1 - pow(-1, _r_value) * _p_value / 1000)
+            self._genes[i] = self._genes[i] * (1 - pow(-1, __r_value) * __p_value / 1000)
 
     def selection(self, father_bot):
         # interbreed two bots (mamas'n'papas) to create a new one
@@ -52,7 +52,7 @@ class Bot:
         _father_bot = father_bot #_myfather=Bot(self.genes)
         _child_bot = Bot(self._genes) #_myfather.assign_bot(_father_bot)
 
-        _separation_mark = random(len(self.genes))
+        _separation_mark = random.random(len(self.genes))
         _child_bot.target = self.target
         _child_bot.eps = self.eps
         _child_bot.success = False
@@ -77,10 +77,10 @@ class Bot:
         # 200'000 units are ready with a million more on a way
         # клонирование бота - присвоение значений параметров Джанго-бота клонам
         # 200000 единиц уже готовы, миллион на подходе
-        self.target = jango_fett_bot.target
+        self._target = jango_fett_bot.target
         self.fitness = jango_fett_bot.fitness
         self.success = jango_fett_bot.success
-        self.eps = jango_fett_bot.eps
+        self._eps = jango_fett_bot.eps
 
         for i in range(len(jango_fett_bot.genes)):
             self._genes[i] = jango_fett_bot.genes[i]
@@ -97,44 +97,76 @@ class Bot:
 
 class Population:
 
-    def __init__(self, _count_bots, max_age, _number_of_genes, _max_value, _min_value, number_of_good, _target, _eps):
+    def __init__(self, count_bots, max_age, number_of_genes, max_value, min_value, number_of_good, target, eps):
         # create a population of bots
         # создает стадо ботов
+        _number_of_good = number_of_good
+        _max_age = max_age
         bots = []
-        for i in range(_count_bots):
-            bots[i] = Bot(_number_of_genes, _max_value, _min_value, _target, _eps)
+        for i in range(count_bots):
+            bots[i] = Bot(number_of_genes, max_value, min_value, target, eps)
 
     def dead_population(self):
         # kill the whole population of bots. Not just the man but the women and the children too
         # убить все стадо ботов. Не только мужчин,но и женщин, и детей тоже
-        for i in range(self.number_of_good, len(self.bots)):
+        for i in range(self._number_of_good, len(self.bots)):
             self.bots[i].dead()
 
     def mutation_population(self):
         # mutate bots of population
-        for i in range(self.number_of_good, len(self.bots)):
-            self.bots[i].mutation
+        # мутация стада ботов
+        for i in range(self._number_of_good, len(self.bots)):
+            self.bots[i].mutation()
 
-    def calculate_age(self):
-        result = False
-        allfitness = 0
-        botfitness = []
-
-        for i in range(len(self.bots)):
-            botfitness[i] = self.bots[i].get_fitness
+    def evolution_chamber(self):
+        __result = False
+        __all_fitness = 0
+        __bot_fitness = []
 
         for i in range(len(self.bots)):
-            if not(self.bots[i].success):
-                allfitness = allfitness + 1 / botfitness[i]
+            __botfitness[i] = self.bots[i].get_fitness
+
+        for i in range(len(self.bots)):
+            if not self.bots[i].success:
+                __all_fitness = __allfitness + 1 / __bot_fitness[i]
             else:
-                result = True
+                __result = True
                 self.winner_bot.assign_bot(self.bots[i])
+                return __result
 
-        for i
+        for i in range(len(self.bots)):
+            self.bots[i].fitness = 1 / __bot_fitness[i] / __all_fitness
+
+        self.sort_fitness()
+        self.dead_population()
+        self.mutation_population()
+        self.resurrection_population()
 
     def execute_population(self):
-        winner_bot = Bot
+        winner_bot = Bot()
+        __result = Bot()
 
-        for i in range(1, self.max_age):
-            if calculate_age:
+        for i in range(1, self._max_age):
+            if evolution_chamber:
+                __result.assign_bot(winner_bot)
+                return __result
 
+        winner_bot.assign_bot(bots[0])
+        return winner_bot
+
+    def resurrection_population(self):
+        for i in range(self._number_of_good, len(self.bots)):
+            __x_value = random.random(self._number_of_good - 1)
+            __y_value = random.random(self._number_of_good - 1)
+
+            bots[i].assign_bot(bots[__x_value].Selection(bots[__y_value]))
+
+    def sort_fitness(self):
+        _bot = Bot()
+        _m = len(self.bots)
+        for i in range(_m):
+            for j in range(_m - i - 2):
+                if bots[j].fitness < bots[j+1].fitness:
+                    _bot.assign_bot(bots[j])
+                    bots[j].assign_bot(bots[j+1])
+                    bots[j+1].assign_bot(_bot)
